060ab3c5174aa968608ff3e848a0c6f9e5b40d4d
David Essien <davidessienshare@gmail.com>
2022-08-18 16:56:27 +0100

    add a pydantic model

--- main.py
+++ main.py
@@ -1,8 +1,16 @@
 from fastapi import FastAPI
 from fastapi.params import Body
 from pydantic import BaseModel
+from typing  import Optional
 
 app = FastAPI()
+
+class Post(BaseModel):
+  title: str
+  content: str
+  published: bool = True
+  rating: Optional[int] = None
+
 
 @app.get("/")
 def root():
@@ -14,6 +22,6 @@ def get_posts():
   return {"data": "posts"}
 
 @app.post("/createpost")
-def create_post(payload: dict = Body(...)):
-  print(payload)
-  return {"new_post": f"title {payload['title']} content {payload['content']}"}
+def create_post(post: Post):
+  print(post.dict())
+  return {"data": post.dict()}
